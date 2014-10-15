46d0239c3d03cf7a8ef776bba3cfce61c85355eb
Jordan Hudgens <jordan@wow.com>
2014-10-14 19:03:58 -0500

    Implemented the keyboard functionality to handle form flow

--- Photog/AuthViewController.swift
+++ Photog/AuthViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet var emailTextField : UITextField?
+    @IBOutlet var passwordTextField : UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.edgesForExtendedLayout = UIRectEdge.None
+        
+        var emailImageView = UIImageView(frame: CGRectMake(0, 0, 50, self.emailTextField!.frame.size.height))
+        emailImageView.image = UIImage(named: "EmailIcon")
+        emailImageView.contentMode = .Center
+        
+        self.emailTextField!.leftView = emailImageView
+        self.emailTextField!.leftViewMode = .Always
+        
+        var passwordImageView = UIImageView(frame: CGRectMake(0, 0, 50, self.passwordTextField!.frame.size.height))
+        passwordImageView.image = UIImage(named: "PasswordIcon")
+        passwordImageView.contentMode = .Center
+        
+        self.passwordTextField!.leftView = passwordImageView
+        self.passwordTextField!.leftViewMode = .Always
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
         self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        self.emailTextField?.becomeFirstResponder()
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -30,6 +54,22 @@ class AuthViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (textField == self.emailTextField) {
+            self.emailTextField?.resignFirstResponder()
+            self.passwordTextField?.becomeFirstResponder()
+        } else if (textField == self.passwordTextField) {
+            self.passwordTextField?.resignFirstResponder()
+            self.authenticate()
+        }
+        
+        return true
+    }
+    
+    func authenticate() {
+        println("Authenticated was clicked")
     }
 
 }
